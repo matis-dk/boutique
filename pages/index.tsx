9b9c39573f123c36789c1149c3be9e5944906aa4
matis-dk <mathiasp50@gmail.com>
2021-05-26 15:10:53 +0200

    Fixed home

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,6 @@
+import { Box, Button, Flex, Grid, Image, Text } from "@theme-ui/components";
 import Head from "next/head";
+import Link from "next/link";
 
 import Header from "../src/layout/Header";
 
@@ -9,7 +11,29 @@ export default function Index() {
         <title>Home - Boutique</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <h1>home</h1>
+      <Grid
+        sx={{
+          height: "80vh",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Flex
+          sx={{
+            justifyContent: "center",
+            alignItems: "center",
+            flexDirection: "column",
+          }}
+        >
+          <Text variant="headline1">Unicorns in fantasyland</Text>
+          <Text variant="manchet">Unicorns and</Text>
+          <Box mt="4">
+            <Link prefetch={false} href={`/products`}>
+              <Button>Se produkter</Button>
+            </Link>
+          </Box>
+        </Flex>
+      </Grid>
     </div>
   );
 }
