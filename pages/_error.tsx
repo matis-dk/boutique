1e5741939d573b8e3299d74ec7802ff2cee71cea
matis-dk <mathiasp50@gmail.com>
2021-06-15 17:21:38 +0200

    Custom error comp2

--- pages/_error.tsx
+++ pages/_error.tsx
@@ -1,15 +1,19 @@
-function Error({ statusCode }) {
-  console.log("EEERROOORR ", statusCode);
+function Error(props) {
+  console.log("EEERROOORR ", props);
   return (
     <p>
-      {statusCode
+      <h1>Something went wrong</h1>
+      {/* {statusCode
         ? `An error ${statusCode} occurred on server`
-        : "An error occurred on client"}
+        : "An error occurred on client"} */}
     </p>
   );
 }
 
 Error.getInitialProps = ({ res, err }) => {
+  console.log("--------------------------");
+  console.log(err);
+  console.log("--------------------------");
   const statusCode = res ? res.statusCode : err ? err.statusCode : 404;
   return { statusCode };
 };
